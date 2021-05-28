0971f843b8affb2556626eee8f29e911e19d3790
Algonight <benjamin.fournet99@gmail.com>
2021-05-28 11:40:22 +0200

    [ADD] get, update, delete services for housings

--- home-exchange-backend/src/main/kotlin/org/isep/homeexchange/core/services/HousingService.kt
+++ home-exchange-backend/src/main/kotlin/org/isep/homeexchange/core/services/HousingService.kt
@@ -1,8 +1,14 @@
 package org.isep.homeexchange.core.services
 
 import org.isep.homeexchange.core.dto.HousingDto
+import org.isep.homeexchange.core.dto.UserDto
 
 interface HousingService {
     fun create(dto: HousingDto): HousingDto
     fun getById(id: Long): HousingDto
+    fun getByUsers(user_id: Long): List<HousingDto>
+    fun getAll(): List<HousingDto>
+    fun updateHousing(dto: HousingDto): HousingDto
+    fun deleteById(id: Long)
+    fun deleteAll()
 }
--- home-exchange-backend/src/main/kotlin/org/isep/homeexchange/core/services/impl/HousingServiceImpl.kt
+++ home-exchange-backend/src/main/kotlin/org/isep/homeexchange/core/services/impl/HousingServiceImpl.kt
@@ -1,10 +1,12 @@
 package org.isep.homeexchange.core.services.impl
 
 import org.isep.homeexchange.core.dto.HousingDto
+import org.isep.homeexchange.core.dto.UserDto
 import org.isep.homeexchange.core.dto.toDao
 import org.isep.homeexchange.core.services.HousingService
 import org.isep.homeexchange.core.services.UserService
 import org.isep.homeexchange.infrastructure.dao.HousingDao
+import org.isep.homeexchange.infrastructure.dao.UserDao
 import org.isep.homeexchange.infrastructure.dao.toDto
 import org.isep.homeexchange.infrastructure.repository.HousingRepository
 import org.springframework.http.HttpStatus
@@ -35,4 +37,41 @@ class HousingServiceImpl(
 
         return housing.get().toDto()
     }
+
+    //Add by Benji
+    override fun getByUsers(user_id: Long): List<HousingDto> {
+        var userDto: UserDto = userService.getById(user_id)
+
+        return userDto.housings
+    }
+
+    override fun getAll(): List<HousingDto> {
+        val housingsDao: List<HousingDao> = housingRepository.findAll();
+
+        return housingsDao.toDto()
+    }
+
+    override fun updateHousing(dto: HousingDto): HousingDto {
+        //A voir si on utilise create a la place
+        return dto
+    }
+
+    override fun deleteById(id: Long) {
+        val housing: Optional<HousingDao> = housingRepository.findById(id)
+
+        if(housing.isEmpty) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "User doesn't exist")
+        }
+
+        housingRepository.deleteById(id)
+    }
+
+    override fun deleteAll() {
+        housingRepository.deleteAll()
+    }
+
+
+    //
+
+
 }
