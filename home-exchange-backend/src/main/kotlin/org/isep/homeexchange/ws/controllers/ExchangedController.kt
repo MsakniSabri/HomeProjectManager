5455979aba3bdcd40cb1e09224d66c3f8214b44a
hypgawan <hypgawan2@gmail.com>
2021-06-02 21:15:20 +0200

    proposition finished

--- home-exchange-backend/src/main/kotlin/org/isep/homeexchange/ws/controllers/ExchangedController.kt
+++ home-exchange-backend/src/main/kotlin/org/isep/homeexchange/ws/controllers/ExchangedController.kt
@@ -1,10 +1,12 @@
 package org.isep.homeexchange.ws.controllers
 
+import org.isep.homeexchange.core.dto.ExchangedDto
 import org.isep.homeexchange.core.services.ExchangedService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("exchanged")
 class ExchangedController(private val exchangedService: ExchangedService) {
-    
+    @PostMapping //✔️
+    fun createExchange(@RequestBody housing_id: Long, user_id: Long, dto: ExchangedDto): ExchangedDto = exchangedService.create(housing_id,user_id,dto)
 }
